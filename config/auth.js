b886d4c6d7575ab176632349f43831cb98a42ffe
Jasu Viding <jviding@raspberrypi>
2016-08-27 22:28:46 +0300

    fixed untracked files

--- config/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-	'facebookAuth' : {
-		'clientID' : '170369080062230',
-		'clientSecret' : '2d2d7cfaf10aac59dfb796692ad44ff2',
-		'callbackURL' : 'http://88.114.187.19/auth/facebook/callback'
-	}
-
-};
