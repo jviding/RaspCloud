a03274fc4feabc46cd59f715823cfd78e04d4810
Jasu Viding <jviding@raspberrypi>
2016-08-27 19:53:23 +0300

    new auth

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
 
 	'facebookAuth' : {
 		'clientID' : '170369080062230',
-		'clientSecret' : '36ca14e9e4e66fae8ee284430b836a80',
+		'clientSecret' : '2d2d7cfaf10aac59dfb796692ad44ff2',
 		'callbackURL' : 'http://88.114.187.19/auth/facebook/callback'
 	}
 
