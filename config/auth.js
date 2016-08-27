0afd67eb013749c0627e7f06434c5a2e38e608a0
jviding <jasu.viding@helsinki.fi>
2016-08-27 19:49:08 +0300

    fixed untracked files

--- config/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-	'facebookAuth' : {
-		'clientID' : '170369080062230',
-		'clientSecret' : '36ca14e9e4e66fae8ee284430b836a80',
-		'callbackURL' : 'http://88.114.187.19/auth/facebook/callback'
-	}
-
-};
