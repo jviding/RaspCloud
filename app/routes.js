7ac8f3366deff6fcca501b5d343934b916559ef0
jviding <jasu.viding@helsinki.fi>
2016-08-27 23:02:56 +0300

    logging

--- app/routes.js
+++ app/routes.js
@@ -35,11 +35,12 @@ module.exports = function (app, passport) {
 //route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
 	console.log('Am I logged in?');
-	console.log(req.isAuthenticated)();
 	//if user is authenticated in the session, carry on
 	if (req.isAuthenticated()) {
+		console.log('Yes');
 		return next();
 	}
+	console.log('No');
 	//if they aren't, redirect them to the home page
 	res.redirect('/');
 };
